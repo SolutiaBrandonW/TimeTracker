52ac3c24cff567e6482158de3cb91d8d88a611fb
Brandon Woodford <brandon_woodford@solutiaconsulting.com>
2020-06-23 09:00:51 -0500

    Add routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
+
 import { ProjectTimeEntryComponent } from './project-time-entry/project-time-entry.component';
+import { AddTimeComponent } from './project-time-entry/add-time/add-time.component';
+import { EditTimeComponent } from './project-time-entry/edit-time/edit-time.component';
+import { ViewTimeComponent } from './project-time-entry/view-time/view-time.component';
 
 const appRoutes: Routes = [
     {path: 'project-time-entry', component: ProjectTimeEntryComponent},
-    {path: '**', component: ProjectTimeEntryComponent}
+    {path: 'project-time-entry/add/:string', component: AddTimeComponent},
+    {path: 'project-time-entry/view/:string', component: ViewTimeComponent}
 ];
 
 @NgModule({
