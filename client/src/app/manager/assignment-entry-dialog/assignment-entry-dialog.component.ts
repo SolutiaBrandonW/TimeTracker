ae9062920d2b6ee03f0d3c25493d0f819f2c98c3
Patrick Balfanz <patrick_balfanz@solutiaconsulting.com>
2020-06-29 11:28:15 -0500

    Add Assignment Entry Dialog

--- client/src/app/manager/assignment-entry-dialog/assignment-entry-dialog.component.ts
+++ client/src/app/manager/assignment-entry-dialog/assignment-entry-dialog.component.ts
@@ -1,54 +1,51 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
-import { ProjectTimeEntryComponent } from 'src/app/project-time-entry/project-time-entry.component';
+
 
 @Component({
-  selector: 'app-project-entry-dialog',
-  templateUrl: './project-entry-dialog.component.html',
-  styleUrls: ['./project-entry-dialog.component.css']
+  selector: 'app-assignment-entry-dialog',
+  templateUrl: './assignment-entry-dialog.component.html',
+  styleUrls: ['./assignment-entry-dialog.component.css']
 })
-export class ProjectEntryDialogComponent implements OnInit {
+export class AssignmentEntryDialogComponent implements OnInit {
 
   form: FormGroup;
-  project_id:number
-  project_name:number
-  start_date:Date
-  end_date:Date
-  description:string
-  status_id:string
+  employee_id: number
+  project_id : number
+  start_date: Date
+  end_date: Date
+  role_id: number
   editing = false;
+
 
   constructor(
     private fb: FormBuilder,
-    private dialogRef: MatDialogRef<ProjectEntryDialogComponent>,
+    private dialogRef: MatDialogRef<AssignmentEntryDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data) {
+    this.employee_id = data.employee_id;
     this.project_id = data.project_id;
-    this.project_name = data.project_name;
     this.start_date = data.start_date;
     this.end_date = data.end_date;
-    this.description = data.description;
-    this.status_id = data.status_id;
+    this.role_id = data.role_id;
     this.editing = true;
   }
 
   ngOnInit(): void {
     this.form = new FormGroup({
+      employee_id:new FormControl,
       project_id:new FormControl,
-      project_name:new FormControl,
       start_date:new FormControl,
       end_date:new FormControl,
-      status_id:new FormControl,
-      description: new FormControl
-    })
+      role_id:new FormControl,
+        })
 
     this.form.patchValue({
+      employee_id: this.employee_id,
       project_id: this.project_id,
-      project_name: this.project_name,
       start_date: this.start_date,
       end_date: this.end_date,
-      description: this.description,
-      status_id: this.status_id
+      role_id: this.role_id
     })
     
   }
@@ -62,4 +59,5 @@ export class ProjectEntryDialogComponent implements OnInit {
   close() {
     this.dialogRef.close();
   }
+
 }
