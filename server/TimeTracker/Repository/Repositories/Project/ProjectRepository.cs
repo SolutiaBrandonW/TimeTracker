e003cba0ef69e16288f46b7963bef08893b7baef
Brandon Woodford <brandon_woodford@solutiaconsulting.com>
2020-06-29 11:01:22 -0500

    Project endpoint created

---
+++ server/TimeTracker/Repository/Repositories/Project/ProjectRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataContracts.EntityFramework;
+using DataContracts.Models;
+using Repository.APIReturnObjects;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Mappers;
+
+namespace Repository.Repositories.Project
+{
+    public class ProjectRepository
+    {
+        public async Task<ReturnAPI<List<ProjectDTO>>> GetProjects()
+        {
+            try
+            {
+                using (var context = new TimeTrackingEntities())
+                {
+
+                    var pro = await context.projects.Select(row => row).ToListAsync();
+
+                    List<ProjectDTO> projectDTOs = new List<ProjectDTO>();
+
+                    foreach (project p in pro)
+                    {
+
+                        ProjectDTO projectDTO = new ProjectDTO();
+                        projectDTO = ProjectMappers.mapToProjectDTO(p, projectDTO);
+                        projectDTOs.Add(projectDTO);
+                    }
+                    return new ReturnAPI<List<ProjectDTO>>("Success", 200, projectDTOs);
+                }
+            }
+            catch (Exception e)
+            {
+                return new ReturnAPI<List<ProjectDTO>>(e.Message, 400, null);
+            }
+        }
+
+    }
+}
---
+++ server/TimeTracker/TimeTracker/Controllers/ProjectController.cs
@@ -0,0 +1,32 @@
+﻿using DataContracts.Models;
+using Repository.APIReturnObjects;
+using Repository.Repositories.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TimeTracker.Controllers
+{   
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Project")]
+    public class ProjectController : ApiController
+    {
+        private readonly ProjectRepository projectRepository;
+        public ProjectController()
+        {
+            this.projectRepository = new ProjectRepository();
+        }
+
+        [Route("GetProjects")]
+        [HttpGet()]
+        public async Task<ReturnAPI<List<ProjectDTO>>> GetProjects()
+        {
+            return await projectRepository.GetProjects();
+        }
+
+    }
+}
